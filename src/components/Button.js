f51a20707112ceee36d31362386a70cf88ec0496
Mohnish Ramanujam <47076690+Mohnish156@users.noreply.github.com>
2021-04-12 13:16:32 +1200

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types'
+const Button = ({color, text, onClick}) => {
+    return (
+
+        <button onClick = {onClick} style={{backgroundColor: color}}className = 'btn'>{text} </button>
+    )
+}
+
+export default Button
